97f87606b272d16a3583ca3368b3d2a1c537066b
Aline Otília <aos_santos@hotmail.com>
2022-11-11 22:26:13 -0300

    Iniciado o Storage

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -7,7 +7,7 @@ import { FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./registro.page.scss'],
 })
 export class RegistroPage implements OnInit {
-  formLogin = this.formBuilder.group({
+  formRegistro = this.formBuilder.group({
     nome: [
       '',
       Validators.compose([Validators.required, Validators.minLength(3)]),
@@ -22,18 +22,67 @@ export class RegistroPage implements OnInit {
     ],
     cpf: [
       '',
-      Validators.compose([Validators.required, Validators.maxLength(11),Validators.minLength(11)]),
+      Validators.compose([
+        Validators.required,
+        Validators.maxLength(11),
+        Validators.minLength(11),
+      ]),
     ],
     senha: [
       '',
-      Validators.compose([Validators.required,Validators.minLength(8)]),
+      Validators.compose([Validators.required, Validators.minLength(8)]),
     ],
     confirmaSenha: [
       '',
       Validators.compose([Validators.required, Validators.minLength(8)]),
     ],
   });
+
+  mensagemErro = {
+    nome:[
+      {tipo: 'required',aviso: 'Digite um nome!'},
+      {tipo: 'minlength', aviso: 'No mínimo 3 dígitos!'},
+    ],
+    email: [
+      { tipo: 'required', aviso: 'Digite um e-mail!' },
+      { tipo: 'email', aviso: 'Tem que ser um e-mail!' },
+      {tipo: 'minlength', aviso: 'No mínimo 6 dígitos!'},
+    ],
+    cpf: [
+      { tipo: 'required', aviso: 'Digite um CPF!' },
+      {tipo: 'minlength', aviso: 'Um CPF tem 11 Dígitos!'},
+      {tipo: 'maxlength', aviso: 'Um CPF tem 11 Dígitos!'},
+    ],
+    senha: [
+      { tipo: 'required', aviso: 'Digite uma senha!' },
+      { tipo: 'minlength', aviso: 'No mínimo 8 dígitos!' },
+    ],
+    confirmaSenha: [
+      { tipo: 'required', aviso: 'Confirme a senha!' },
+      { tipo: 'minlength', aviso: 'No mínimo 8 dígitos!' },
+    ],
+  };
   constructor(private formBuilder: FormBuilder) {}
+
+  get nome() {
+    return this.formRegistro.get('nome');
+  }
+
+  get cpf() {
+    return this.formRegistro.get('cpf');
+  }
+
+  get email() {
+    return this.formRegistro.get('email');
+  }
+
+  get senha() {
+    return this.formRegistro.get('senha');
+  }
+
+  get confirmaSenha() {
+    return this.formRegistro.get('confirmaSenha');
+  }
 
   ngOnInit() {}
 }
